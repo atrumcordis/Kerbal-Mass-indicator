3303d33e1a64a04796eddf1e81c6704d410fc785
atrumcordis <williebru@gmail.com>
2015-08-19 22:29:05 -0500

    Made a project.

---
+++ KerbalMassIndicator/KerbalMassIndicator/KerbalMassIndicator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//This unfinished piece of software made by: William Brubaker
+// Contact: williebru@gmail.com 
+
+
+// Trying to learn how to draw things in flight. Preferably the CoM sphere thing from the VAB.
+ 
+namespace KerbalMassIndicator
+{
+    public class ModuleMassIndicator : PartModule
+    {
+
+    }
+}
