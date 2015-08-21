63abc6906b120935320fce66fc8b4788ad43bdaa
atrumcordis <williebru@gmail.com>
2015-08-20 21:04:36 -0500

    Found editor marker classes

--- KerbalMassIndicator/KerbalMassIndicator/KerbalMassIndicator.cs
+++ KerbalMassIndicator/KerbalMassIndicator/KerbalMassIndicator.cs
@@ -11,8 +11,23 @@ using System.Threading.Tasks;
  
 namespace KerbalMassIndicator
 {
-    public class ModuleMassIndicator : PartModule
+    public class ModuleMassIndicator
     {
+        //EditorMarker_CoM();   This is what the CoM button in editor does. I need more c# review. Not sure how to use/call/overload this in a useful way.
+        //EditorMarker definition:
+        /*
+                using UnityEngine;
 
+                public class EditorMarker : MonoBehaviour
+                    {
+                        public GameObject dirMarkerObject;
+                        public GameObject posMarkerObject;
+
+                        public EditorMarker();
+
+                        protected virtual Vector3 UpdateDirection();
+                        protected virtual Vector3 UpdatePosition();
+                    }
+                    */
     }
 }
